318f5d2093c41a70bfea1199f498309c274ab835
Mahender Kumar <mkayrajput8@gmail.com>
2023-08-04 22:53:14 +0530

    Code formating

--- app.js
+++ app.js
@@ -1,30 +1,39 @@
-require('dotenv').config()
-const express = require("express")
+require("dotenv").config();
+const express = require("express");
 const app = express();
-const path = require('path');
-const generateImage = require("./config.js");
-const { error } = require('console');
+const path = require("path");
+const { Configuration, OpenAIApi } = require("openai");
 
 // set the view engine to ejs
-app.use(express.static('public'))
+app.use(express.static("public"));
 app.use(express.json());
 
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
 
+const openai = new OpenAIApi(configuration);
 
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, '/index.html'));
-})
-app.post("/", async(req, res) => {
-    const userPrompt = (req.body.userPrompt)
-    const response = await generateImage(userPrompt)
-    res.send(response)
-})
+  res.sendFile(path.join(__dirname, "/index.html"));
+});
 
-
-
+app.post("/", async (req, res) => {
+  const userPrompt = req.body.userPrompt;
+  try {
+    const response = await openai.createImage({
+      prompt: userPrompt,
+      n: 3,
+      size: "1024x1024",
+    });
+    return res.status(201).json(response);
+  } catch (err) {
+    return res.status(502).json({ message: "Something went wrong." });
+  }
+});
 
 //.............server..........................
 
 app.listen(process.env.PORT || 5500, () => {
-    console.log("Server is Running...")
-})
+  console.log("Server is Running...");
+});
